87cc7e2fd99a57584c41662756ac58d7a2a2d77e
KillenNolan <50820655+KillenNolan@users.noreply.github.com>
2021-11-28 20:55:49 +0800

    Add files via upload
    the  way of use Notepad++ for C#(for sample case)

---
+++ hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+//自定义快捷键Shift+F5运行简单的C#代码。
+//cmd /K C:\windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:exe /out:$(FULL_CURRENT_PATH).exe $(FULL_CURRENT_PATH) && $(FULL_CURRENT_PATH).exe
+
+
+public class Program{
+	static void Main(){
+		Console.Write("Hello");
+	}
+}
